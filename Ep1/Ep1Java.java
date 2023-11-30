91f41657dedbc7f638fdd185c9bf301776c64edd
Rafael-Balocco <131701269+Rafael-Balocco@users.noreply.github.com>
2023-11-30 02:26:55 +0000

    fixing errors

--- Ep1/Ep1Java.java
+++ Ep1/Ep1Java.java
@@ -1,27 +1,38 @@
-// Classe que estende Thread para criar threads que exibirão "Hello world"
-class HelloWorldThread extends Thread {
-    public void run() {
-        // Exibe uma mensagem com o nome da thread atual
+  // Classe que estende Thread para criar threads que exibirão "Hello world"
+  class HelloWorldThread extends Thread {
+      public void run() {
+          // Exibe uma mensagem com o nome da thread atual
+        
+          System.out.println("Hello World from thread: " + Thread.currentThread().getName());
       
-        System.out.println("Hello World from thread: " + Thread.currentThread().getName());
-    
-    } //Ao finalizar método Run, finalizamos essa thread
-}
-
-// Classe principal que contém o método main
-public class Ep1Java {
-    public static void main(String[] args) {
-        int numThreads = 10; // Número de threads a serem criadas
-
-        // Loop para criar e iniciar várias threads
-        for (int i = 0; i < numThreads; i++) {
-
-            // Cria uma nova instância da classe HelloWorldThread
-            Thread thread = new HelloWorldThread();
-            thread.setName("" + i);
-
-            // Inicia a thread i, invocando o método run()
-            thread.start();
+      } //Ao finalizar método Run, finalizamos essa thread
+  }
+  
+  // Classe principal que contém o método main
+  public class Ep1Java {
+      public static void main(String[] args) {
+          int numThreads = 10; // Número de threads a serem criadas
+  
+          // Loop para criar e iniciar várias threads
+          for (int i = 0; i < numThreads; i++) {
+  
+              // Cria uma nova instância da classe HelloWorldThread
+              Thread thread = new HelloWorldThread();
+              thread.setName("" + i);
+  
+              // Inicia a thread i, invocando o método run()
+              thread.start();
+          }
+        for (Thread t : Thread.getAllStackTraces().keySet()) {
+            // Verifica se a thread não é a thread principal (atual)
+            if (!t.equals(Thread.currentThread())) {
+            try {
+                // Espera que a thread termine sua execução
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
+  }
 }
